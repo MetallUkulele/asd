d3d5e13fefe0e41f13e6634b9cfee73928cd33fc
Igor Evdokimov <evdokimov048@gmail.com>
2022-05-21 08:55:04 +0300

    3 task ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
-function compareArrays(arr1, arr2) {
-  let result;
+function compareArrays(arr1, arr2) {  
 
-  // Ваш код
+  const result = arr2.every(value => {
+    return  arr1.length === arr2.length && arr1.indexOf(value) === arr2.indexOf(value);
+  })
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr
+  
+  .filter(value => value > 0 && value % 3 === 0)
 
-  // Ваш код
+  .map (value => {
+    return value * 10;
+  }); 
 
   return resultArr; // array
 }
