0b3be88709beb1cd9a282462b586b84842c8f3c4
Igor Evdokimov <evdokimov048@gmail.com>
2022-05-31 08:44:45 +0300

    classes

---
+++ 5.classes/task.js
@@ -0,0 +1,83 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;    
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  set state(_state) {
+    if (_state < 0) {
+      return this._state = 0;
+    } else if (_state > 100){
+      return this._state = 100;
+    } else {
+      return this._state = _state;
+    }    
+  } 
+
+  fix() {        
+    return this.state = this._state * 1.5;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount, state) {
+    super(name, releaseDate, pagesCount, state);
+    this.type = 'magazine';
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount, state) {
+    super(name, releaseDate, pagesCount, state);
+    this.author = author;
+    this.type = 'book';
+  }
+}
+
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount, state) {
+    super(author, name, releaseDate, pagesCount, state);
+    this.type = 'novel';
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount, state) {
+    super(author, name, releaseDate, pagesCount, state);
+    this.type = 'fantastic';
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount, state) {
+    super(author, name, releaseDate, pagesCount, state);
+    this.type = 'detective';
+  }
+}
+
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+   this.books.find((type, value) => this.book)
+  }
+}
+
+
+
