9d6c6d4a0657c2599c26c290ed29fee9832bdb4c
Igor Yevdokimov <evdokimov048@gmail.com>
2022-06-08 16:16:28 +0300

    exceptions ready

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,53 @@
+function parseCount(value) {
+  parseResult = Number.parseInt(value);
+  if (isNaN(parseResult)) throw new Error("Невалидное значение");
+
+  return parseResult;  
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (err) {
+    return err;
+  }
+
+
+}
+
+class Triangle {
+  constructor(a,b,c) {
+    if ((a + b) < c || (a + c) < b || (b + c) < a) {
+      const errorTriangle = new Error("Треугольник с такими сторонами не существует")
+      throw errorTriangle;
+    };
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  getPerimeter() {    
+    return this.a + this.b + this.c;
+  }
+
+  getArea() {
+    let p = (this.a + this.b + this.c) / 2;
+    let area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return Number(area.toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return  new Triangle(a, b, c);
+  } catch(err) {
+      return {
+        getPerimeter() {
+          return 'Ошибка! Треугольник не существует';
+        },
+        getArea() {
+          return 'Ошибка! Треугольник не существует';
+        }
+      }
+    }
+  }
